b6e3c3471043248a50f9e2d4ee0ca161631469ae
Ranjeet Kumar <rk2505152@gmail.com>
2023-12-01 01:17:21 +0530

    updated

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express=require("express");
+const cors = require('cors');
 const app = express();
 const dbConfig = require('./db.js');
 const roomsRoute=require('./routes/roomsRoute.js');
 const usersRoute=require('./routes/usersRoute.js');
+const corsOptions = {
+      origin: true,
+      credentials: true,
+ };
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/rooms',roomsRoute)
 app.use('/api/users',usersRoute)
